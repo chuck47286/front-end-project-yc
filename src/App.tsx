03031d1383d8b6b86a63a9e2089da71ab206d411
chuck47286 <chuck472861399@163.com>
2024-07-11 16:39:05 +0800

    [feat] tsx父子组件传参

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,11 @@
 import React from 'react';
+import List1 from './list1';
 
 function App() {
-  const questionList = [
-    { id: 'q1', title: '问卷1', isPublished: false },
-    { id: 'q2', title: '问卷2', isPublished: true },
-    { id: 'q3', title: '问卷3', isPublished: false },
-    { id: 'q4', title: '问卷4', isPublished: true },
-  ];
-
-  function edit(id: string) {
-    console.log('edit ', id);
-  }
-
   return (
-    <div>
-      <h1>问卷列表页</h1>
-      <div>
-        {questionList.map((question) => {
-          const { id, title, isPublished } = question;
-          return (
-            <div key={id}>
-              <strong>{title}</strong>
-              {/* 不换行空格 */}
-              &nbsp;
-              {/* 判断 */}
-              {isPublished ? (
-                <span style={{ color: 'green' }}>已发布</span>
-              ) : (
-                <span>未发布</span>
-              )}
-              &nbsp;
-              {/* 编辑问卷 */}
-              <button
-                onClick={() => {
-                  edit(id);
-                }}
-              >
-                编辑问卷
-              </button>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      <List1 />
+    </>
   );
 }
 
---
+++ src/component/questionCard.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react';
+import './questionCard.css';
+
+type propsType = {
+  id: string;
+  title: string;
+  isPublished: boolean;
+};
+
+const QuestionCard: FC<propsType> = (props) => {
+  const { id, title, isPublished } = props;
+
+  function edit(id: string) {
+    console.log('edit ', id);
+  }
+
+  return (
+    <div key={id} className="list-item">
+      <strong>{title}</strong>
+      {/* 不换行空格 */}
+      &nbsp;
+      {/* 判断 */}
+      {isPublished ? (
+        <span style={{ color: 'green' }}>已发布</span>
+      ) : (
+        <span>未发布</span>
+      )}
+      &nbsp;
+      {/* 编辑问卷 */}
+      <button
+        onClick={() => {
+          edit(id);
+        }}
+      >
+        编辑问卷
+      </button>
+    </div>
+  );
+};
+
+export default QuestionCard;
